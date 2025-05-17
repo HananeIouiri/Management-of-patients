9afc042113314898890125b31c1290b018982f82
hanane <hananeiouiri1@gmail.com>
2025-05-17 12:56:23 +0100

    first commit

---
+++ src/main/java/ma/enset/patientsapp/PatientsappApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.patientsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsappApplication.class, args);
+	}
+
+}
